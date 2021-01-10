bfce4f73159e5682cca2a43864f69cfadb0eca72
Dahunsi John <dahunsijohn@gmail.com>
2021-01-10 02:46:51 +0100

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,24 @@ const express = require('express');
 
 const app = express();
 
+
+
 app.get('/', (req, res) => res.send('API Running'));
+
+app.get('/api/rates', (req, res)=>{
+ res.json({
+  "results": {
+   "base": "CZK",
+   "date": (new Date()).toLocaleDateString(),
+   "rates": {
+            "EUR": 0.0377244605,
+            "GBP": 0.033795458,
+            "USD": 0.044824204
+
+   } 
+  }
+ });
+});
 
 const PORT = process.env.PORT || 5000;
 
